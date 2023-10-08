03b2bba8641a04ca523be4f50146e66788e28753
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2023-10-08 11:37:30 +0300

    add lw1-3

---
+++ lab1/SomeThreads/SomeThreads/SomeThreads.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <windows.h>
+#include <string>
+#include <iostream>
+struct Point
+{
+	int x;
+	int y;
+};
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+	Point point = *(Point*)(lpParam);
+	ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+int main(int argc, char* argv[])
+{
+	Point point{0,0};
+	Point* pointPtr = &point;
+	int threadCount = atoi(argv[1]);
+	HANDLE* handles = new HANDLE[threadCount];
+	for (int i = 0; i < threadCount; i++)
+	{
+		handles[i] = CreateThread(NULL, 0, &ThreadProc, i, CREATE_SUSPENDED, NULL);
+	}
+	// запуск двух потоков
+	for (int i = 0; i < threadCount; i++)
+	{
+		ResumeThread(handles[i]);
+	}
+	// ожидание окончания работы потоков
+	WaitForMultipleObjects(threadCount, handles, true, INFINITE);
+
+	return 0;
+}
---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,47 @@
+﻿
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include<ctime>
+
+using namespace std;
+
+clock_t start;
+
+ofstream out("out.txt");
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+	int number = *(int*)(lpParam);
+	for (int i = 0; i < 20; i++)
+	{
+		for (int i = 0; i < 1000000; i++)
+		{
+			int a = 1;
+		}
+		string str = " " + to_string(number) + " " + to_string(clock()-start) + "\n";
+		out << str;
+	}
+	ExitThread(0); // функция устанавливает код завершения потока в 0
+}
+int main(int argc, char* argv[])
+{
+	system("pause");
+	start = clock();
+	int one = 1;
+	int two = 2;
+	HANDLE* handles = new HANDLE[2];
+	handles[0] = CreateThread(NULL, 0, &ThreadProc, &one, CREATE_SUSPENDED, NULL);
+	handles[1] = CreateThread(NULL, 0, &ThreadProc, &two, CREATE_SUSPENDED, NULL);
+	SetThreadPriority(handles[0], 0);
+	SetThreadPriority(handles[1], 1);
+	// запуск двух потоков
+	for (int i = 0; i < 2; i++)
+	{
+		ResumeThread(handles[i]);
+	}
+	// ожидание окончания работы потоков
+	WaitForMultipleObjects(2, handles, true, INFINITE);
+
+	return 0;
+}
